ca75fe3725f75d0f06a0668e56168ebf486a2e10
Molly Wilson <molly_wilson1@outlook.com>
2021-05-15 14:43:13 -0500

    logic for generating license information

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,54 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//supported licenses: Apache, BSD, GPL, MIT
 
-// TODO: Create a function that returns the license link
+// a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseBadge = license => {
+  if (!license) return "";
+
+  const licenseCheck = license.toLowerCase();
+
+  if (licenseCheck === "apache" || licenseCheck === "gpl") {
+
+    return "https://img.shields.io/badge/license-" + license + "-blue";
+  }
+
+  return "https://img.shields.io/badge/license-" + license + "-brightgreen";
+};
+
+// a function that returns the license link
+// If there is no license, return an empty string
+const renderLicenseLink = license => {
+  license = license.toLowerCase();
+  switch (license) {
+    case "apache":
+      return "https://www.apache.org/licenses/";
+      break;
+    case "bsd":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    case "gpl":
+      return "https://www.gnu.org/licenses/gpl-3.0.en.html";
+      break;
+    case "mit":
+      return "https://opensource.org/licenses/MIT";
+      break;
+    default:
+      return "";
+  }
+
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = license => {
+  if(!license) return "";
+
+  const link = renderLicenseLink(license);
+  
+  return `## License
+  This project is covered under the ${license} license. For more detail, please go to [${link}](${link}).`
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -18,3 +58,8 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+const mockData = "mit";
+console.log(renderLicenseBadge(mockData));
+console.log(renderLicenseLink(mockData));
+console.log(renderLicenseSection(mockData));
